f84edaf12be8c4bfc6e240001c8c3b5a43dd58c3
yxlllc <llc1995@sina.com>
2024-12-18 22:50:45 +0800

    optimize dataloader

--- lib/dataset.py
+++ lib/dataset.py
@@ -7,6 +7,47 @@ import torch.utils.data
 from tqdm import tqdm
 
 
+def traverse_dir(
+        root_dir,
+        extensions,
+        amount=None,
+        str_include=None,
+        str_exclude=None,
+        is_pure=True,
+        is_sort=True,
+        is_ext=True):
+
+    file_list = []
+    cnt = 0
+    for root, _, files in os.walk(root_dir):
+        for file in files:
+            if any([file.endswith(f".{ext}") for ext in extensions]):
+                # path
+                mix_path = os.path.join(root, file)
+                pure_path = mix_path[len(root_dir)+1:] if is_pure else mix_path
+
+                # amount
+                if (amount is not None) and (cnt == amount):
+                    if is_sort:
+                        file_list.sort()
+                    return file_list
+                
+                # check string
+                if (str_include is not None) and (str_include not in pure_path):
+                    continue
+                if (str_exclude is not None) and (str_exclude in pure_path):
+                    continue
+                
+                if not is_ext:
+                    ext = pure_path.split('.')[-1]
+                    pure_path = pure_path[:-(len(ext)+1)]
+                file_list.append(pure_path)
+                cnt += 1
+    if is_sort:
+        file_list.sort()
+    return file_list
+    
+    
 class VocalRemoverTrainingSet(torch.utils.data.Dataset):
 
     def __init__(self, 
@@ -26,16 +67,6 @@ class VocalRemoverTrainingSet(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.training_set)
-
-    def aggressively_remove_vocal(self, X, y):
-        X_mag = np.abs(X)
-        y_mag = np.abs(y)
-        v_mag = X_mag - y_mag
-        v_mag *= v_mag > y_mag
-
-        y_mag = np.clip(y_mag - v_mag * self.reduction_weight, 0, np.inf)
-
-        return y_mag * np.exp(1.j * np.angle(y))
 
     def do_crop(self, X_path, y_path, duration):
         start_time = random.uniform(0, duration - self.waveform_sec - 0.1)
@@ -105,6 +136,7 @@ class VocalRemoverTrainingSet(torch.utils.data.Dataset):
             X, y = self.do_mixup(X, y)
         return X, y
 
+
 class VocalRemoverValidationSet(torch.utils.data.Dataset):
     def __init__(self, validation_set, sr):
         self.validation_set = validation_set
@@ -138,21 +170,10 @@ class VocalRemoverValidationSet(torch.utils.data.Dataset):
 
 
 def make_pair(mix_dir, inst_dir):
-    input_exts = ['.wav', '.m4a', '.mp3', '.mp4', '.flac']
-
-    X_list = sorted([
-        os.path.join(mix_dir, fname)
-        for fname in os.listdir(mix_dir)
-        if os.path.splitext(fname)[1] in input_exts
-    ])
-    y_list = sorted([
-        os.path.join(inst_dir, fname)
-        for fname in os.listdir(inst_dir)
-        if os.path.splitext(fname)[1] in input_exts
-    ])
-
+    input_exts = ['wav', 'flac']
+    X_list = traverse_dir(mix_dir, input_exts, is_pure=False, is_sort=True, is_ext=True)
+    y_list = traverse_dir(inst_dir, input_exts, is_pure=False, is_sort=True, is_ext=True)
     filelist = list(zip(X_list, y_list))
-
     return filelist
 
 
@@ -188,94 +209,3 @@ def train_val_split(dataset_dir, split_mode, val_num, val_filelist):
         )
 
     return train_filelist, val_filelist
-
-
-def make_padding(width, cropsize, offset):
-    left = offset
-    roi_size = cropsize - offset * 2
-    if roi_size == 0:
-        roi_size = cropsize
-    right = roi_size - (width % roi_size) + left
-
-    return left, right, roi_size
-
-
-def make_training_set(filelist, sr, hop_length, n_fft, device):
-    ret = []
-    window = torch.hann_window(n_fft).to(device)
-    for X_path, y_path in tqdm(filelist):
-        X, y, X_cache_path, y_cache_path = spec_utils.cache_or_load(
-            X_path, y_path, sr, hop_length, n_fft, window, read_cache=False,
-        )
-        ret.append([X_cache_path, y_cache_path])
-
-    return ret
-
-
-def make_validation_set(filelist, cropsize, sr, hop_length, n_fft, offset, device):
-    patch_list = []
-    patch_dir = 'cs{}_sr{}_hl{}_nf{}_of{}'.format(cropsize, sr, hop_length, n_fft, offset)
-    os.makedirs(patch_dir, exist_ok=True)
-    window = torch.hann_window(n_fft).to(device)
-    
-    for X_path, y_path in tqdm(filelist):
-        basename = os.path.splitext(os.path.basename(X_path))[0]
-
-        X, y, _, _ = spec_utils.cache_or_load(X_path, y_path, sr, hop_length, n_fft, window)
-
-        l, r, roi_size = make_padding(X.shape[2], cropsize, offset)
-        X_pad = np.pad(X, ((0, 0), (0, 0), (l, r)), mode='constant')
-        y_pad = np.pad(y, ((0, 0), (0, 0), (l, r)), mode='constant')
-
-        len_dataset = int(np.ceil(X.shape[2] / roi_size))
-        for j in range(len_dataset):
-            outpath = os.path.join(patch_dir, '{}_p{}.npz'.format(basename, j))
-            start = j * roi_size
-            if not os.path.exists(outpath):
-                np.savez(
-                    outpath,
-                    X=X_pad[:, :, start:start + cropsize],
-                    y=y_pad[:, :, start:start + cropsize]
-                )
-            patch_list.append(outpath)
-
-    return patch_list
-
-
-def get_oracle_data(X, y, oracle_loss, oracle_rate, oracle_drop_rate):
-    k = int(len(X) * oracle_rate * (1 / (1 - oracle_drop_rate)))
-    n = int(len(X) * oracle_rate)
-    indices = np.argsort(oracle_loss)[::-1][:k]
-    indices = np.random.choice(indices, n, replace=False)
-    oracle_X = X[indices].copy()
-    oracle_y = y[indices].copy()
-
-    return oracle_X, oracle_y, indices
-
-
-if __name__ == "__main__":
-    import sys
-    import utils
-
-    mix_dir = sys.argv[1]
-    inst_dir = sys.argv[2]
-    outdir = sys.argv[3]
-
-    os.makedirs(outdir, exist_ok=True)
-
-    filelist = make_pair(mix_dir, inst_dir)
-    for mix_path, inst_path in tqdm(filelist):
-        mix_basename = os.path.splitext(os.path.basename(mix_path))[0]
-
-        X_spec, y_spec, _, _ = spec_utils.cache_or_load(
-            mix_path, inst_path, 44100, 1024, 2048
-        )
-
-        X_mag = np.abs(X_spec)
-        y_mag = np.abs(y_spec)
-        v_mag = X_mag - y_mag
-        v_mag *= v_mag > y_mag
-
-        outpath = '{}/{}_Vocal.jpg'.format(outdir, mix_basename)
-        v_image = spec_utils.spectrogram_to_image(v_mag)
-        utils.imwrite(outpath, v_image)
